d62a3edf4753c0011a807e9e03505a9787c7c7f3
prajithravisankar <prajithravisankar@gmail.com>
2026-02-18 00:09:26 -0500

    implemented document length normalization. updated __add_document function to track document lengths. Updated save and load methods for doc_lengths and cache files. implemented a private helper to get average doc length. Now the updated get_bm25_tf includes document length noramlization formula. Tested with different keywords works as expected

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -2,7 +2,6 @@
 
 import argparse
 
-from lib.constants import BM25_K1
 from lib.keyword_search import (
     bm25_idf_command,
     bm25_tf_command,
@@ -12,6 +11,7 @@ from lib.keyword_search import (
     tf_command,
     tfidf_command,
 )
+from lib.search_utils import BM25_B, BM25_K1
 
 
 def main() -> None:
@@ -55,6 +55,9 @@ def main() -> None:
     bm25_tf_parser.add_argument(
         "k1", type=float, nargs="?", default=BM25_K1, help="Tunable BM25 K1 parameter"
     )
+    bm25_tf_parser.add_argument(
+        "b", type=float, nargs="?", default=BM25_B, help="Tunable BM25 b parameter"
+    )
 
     args = parser.parse_args()
 
@@ -83,7 +86,7 @@ def main() -> None:
             bm25idf = bm25_idf_command(args.term)
             print(f"BM25 IDF score of '{args.term}': {bm25idf:.2f}")
         case "bm25tf":
-            bm25tf = bm25_tf_command(args.doc_id, args.term, args.k1)
+            bm25tf = bm25_tf_command(args.doc_id, args.term, args.k1, args.b)
             print(
                 f"BM25 TF score of '{args.term}' in document '{args.doc_id}': {bm25tf:.2f}"
             )
--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -4,8 +4,9 @@ import pickle
 import string
 from collections import Counter, defaultdict
 
-from lib.constants import BM25_K1
 from lib.search_utils import (
+    BM25_B,
+    BM25_K1,
     CACHE_DIR,
     DEFAULT_SEARCH_LIMIT,
     load_movies,
@@ -18,9 +19,11 @@ class InvertedIndex:
     def __init__(self) -> None:
         self.index = defaultdict(set)
         self.docmap: dict[int, dict] = {}
+        self.doc_lengths = {}
         self.index_path = os.path.join(CACHE_DIR, "index.pkl")
         self.docmap_path = os.path.join(CACHE_DIR, "docmap.pkl")
         self.tf_path = os.path.join(CACHE_DIR, "term_frequencies.pkl")
+        self.doc_lengths_path = os.path.join(CACHE_DIR, "doc_lengths.pkl")
         self.term_frequencies = defaultdict(Counter)
 
     def build(self) -> None:
@@ -39,6 +42,8 @@ class InvertedIndex:
             pickle.dump(self.docmap, f)
         with open(self.tf_path, "wb") as f:
             pickle.dump(self.term_frequencies, f)
+        with open(self.doc_lengths_path, "wb") as f:
+            pickle.dump(self.doc_lengths, f)
 
     def load(self) -> None:
         with open(self.index_path, "rb") as f:
@@ -47,6 +52,8 @@ class InvertedIndex:
             self.docmap = pickle.load(f)
         with open(self.tf_path, "rb") as f:
             self.term_frequencies = pickle.load(f)
+        with open(self.doc_lengths_path, "rb") as f:
+            self.doc_lengths = pickle.load(f)
 
     def get_documents(self, term: str) -> list[int]:
         doc_ids = self.index.get(term, set())
@@ -54,6 +61,7 @@ class InvertedIndex:
 
     def __add_document(self, doc_id: int, text: str) -> None:
         tokens = tokenize_text(text)
+        self.doc_lengths[doc_id] = len(tokens)
         for token in set(tokens):
             self.index[token].add(doc_id)
         self.term_frequencies[doc_id].update(tokens)
@@ -74,27 +82,38 @@ class InvertedIndex:
         term_doc_count = len(self.index[token])
         return math.log((doc_count + 1) / (term_doc_count + 1))
 
+    def get_bm25_idf(self, term: str) -> float:
+        tokens = tokenize_text(term)
+        if len(tokens) != 1:
+            raise ValueError("term must be a single token")
+        token = tokens[0]
+        doc_count = len(self.docmap)
+        term_doc_count = len(self.index[token])
+        return math.log((doc_count - term_doc_count + 0.5) / (term_doc_count + 0.5) + 1)
+
+    def __get_avg_doc_length(self) -> float:
+        total_documents = len(self.doc_lengths)
+        if total_documents == 0:
+            return 0
+        total_length = sum(self.doc_lengths.values())
+        return total_length / total_documents
+
+    def get_bm25_tf(
+        self, doc_id: int, term: str, k1: float = BM25_K1, b=BM25_B
+    ) -> float:
+        doc_length = self.doc_lengths.get(doc_id, 0)
+        avg_doc_length = self.__get_avg_doc_length()
+        if avg_doc_length == 0:
+            return 0.0
+        length_norm = 1 - b + b * (doc_length / avg_doc_length)
+        tf = self.get_tf(doc_id, term)
+        tf_component = (tf * (k1 + 1)) / (tf + k1 * length_norm)
+        return tf_component
+
     def get_tf_idf(self, doc_id: int, term: str) -> float:
         tf = self.get_tf(doc_id, term)
         idf = self.get_idf(term)
         return tf * idf
-
-    def get_bm25_idf(self, term: str) -> float:
-        token = tokenize_text(term)
-        if len(token) != 1:
-            raise Exception("token length > 1")
-        target = token[0]
-        N = len(self.docmap)
-        df = len(self.index.get(target, set()))
-
-        numerator = N - df + 0.5
-        denominator = df + 0.5
-        return math.log((numerator / denominator) + 1)
-
-    def get_bm25_tf(self, doc_id, term, k1=BM25_K1):
-        tf = self.get_tf(doc_id, term)
-        saturated_tf_score = (tf * (k1 + 1)) / (tf + k1)
-        return saturated_tf_score
 
 
 def build_command() -> None:
@@ -153,16 +172,18 @@ def tf_command(doc_id: int, term: str) -> int:
     return idx.get_tf(doc_id, term)
 
 
+def bm25_tf_command(
+    doc_id: int, term: str, k1: float = BM25_K1, b: float = BM25_B
+) -> float:
+    idx = InvertedIndex()
+    idx.load()
+    return idx.get_bm25_tf(doc_id, term, k1, b)
+
+
 def idf_command(term: str) -> float:
     idx = InvertedIndex()
     idx.load()
     return idx.get_idf(term)
-
-
-def tfidf_command(doc_id: int, term: str) -> float:
-    idx = InvertedIndex()
-    idx.load()
-    return idx.get_tf_idf(doc_id, term)
 
 
 def bm25_idf_command(term: str) -> float:
@@ -171,7 +192,7 @@ def bm25_idf_command(term: str) -> float:
     return idx.get_bm25_idf(term)
 
 
-def bm25_tf_command(doc_id, term, k1=BM25_K1):
+def tfidf_command(doc_id: int, term: str) -> float:
     idx = InvertedIndex()
     idx.load()
-    return idx.get_bm25_tf(doc_id, term, k1)
+    return idx.get_tf_idf(doc_id, term)
