300faa4631ecfe99e33f1980ed84e68b52ffc0d3
prajithravisankar <prajithravisankar@gmail.com>
2026-04-30 07:11:36 -0400

    cross encoder re-ranking: implemented a cross encoder reranking for RRF search using ms-marco-TinyBERT-L2-v2

--- cli/lib/reranking.py
+++ cli/lib/reranking.py
@@ -5,6 +5,8 @@ from time import sleep
 from dotenv import load_dotenv
 from google import genai
 
+from sentence_transformers import CrossEncoder
+
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 if not api_key:
@@ -87,6 +89,21 @@ def llm_rerank_batch(query: str, documents: list[dict], limit: int = 5) -> list[
     return reranked[:limit]
 
 
+def cross_encoder_result(query: str, documents: list[dict], limit: int = 5):
+    pairs = []
+    cross_encoder = CrossEncoder("cross-encoder/ms-marco-TinyBERT-L2-v2")
+    for doc in documents:
+        pairs.append([query, f"{doc.get('title', '')} - {doc.get('document', '')}"])
+    scores = cross_encoder.predict(pairs)
+    encoder_doc_score = []
+    for i in range(len(documents)):
+        encoder_doc_score.append(
+            {**documents[i], "cross_encoder_score": float(scores[i])}
+        )
+    encoder_doc_score.sort(key=lambda x: x["cross_encoder_score"], reverse=True)
+    return encoder_doc_score[:limit]
+
+
 def rerank(
     query: str, documents: list[dict], method: str = "batch", limit: int = 5
 ) -> list[dict]:
@@ -94,5 +111,7 @@ def rerank(
         return llm_rerank_individual(query, documents, limit)
     if method == "batch":
         return llm_rerank_batch(query, documents, limit)
+    if method == "cross_encoder":
+        return cross_encoder_result(query, documents, limit)
     else:
         return documents[:limit]
